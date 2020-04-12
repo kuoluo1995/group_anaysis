99af716cc24a31caa7e4f8d5f494d651d05b4a64
kuoluo1995 <kuoluo1995@gmail.com>
2020-04-13 01:54:44 +0800

    change socket file place

--- _test_service.py
+++ _test_service.py
@@ -40,8 +40,9 @@ if __name__ == '__main__':
 
     start = timeit.default_timer()
     ranges = {'关系': {NodeLabels['association']: 0}, '亲属': {EdgeLabels['kin']: 1}}
-    person = get_relation_person_by_name('狄仁杰', ranges)
-    print('查询王安石耗时:{}'.format(timeit.default_timer() - start))
+    name = '蔡京'
+    person = get_relation_person_by_name(name, ranges)
+    print('查询{}耗时:{}'.format(name, timeit.default_timer() - start))
     all_relation_person = [_person_id for _person_id, types in person.items()]
 
     GRAPH_DAO.start_connect()
@@ -56,7 +57,7 @@ if __name__ == '__main__':
     print('查询的人:{}'.format(person_ids))
     start = timeit.default_timer()
     all_topic_ids, topic_id2sentence_id2position1d, topic_pmi, person_id2position2d, node_dict, edge_dict, topic_id2lrs, all_sentence_dict, topic_id2sentence_ids2vector, person_id2sentence_ids = get_topics_by_person_ids(
-        person_ids, populate_ratio=0.3, max_topic=10)
+        person_ids, populate_ratio=0.5, max_topic=10, min_sentence=5)
     print(len(all_topic_ids))
     print('查询所有topic的相关性:{}'.format(timeit.default_timer() - start))
     all_sentence_ids = {_id for _id in all_sentence_dict.keys()}
--- group_anaysis/routing.py
+++ group_anaysis/routing.py
@@ -1,10 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from services import socket_service
+from group_anaysis import socket_view
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
-        URLRouter([path('socket_search_topics_by_person_ids/', socket_service.SocketSearchTopicsByPersonIds), ])
+        URLRouter([path('socket_search_topics_by_person_ids/', socket_view.SocketSearchTopicsByPersonIds), ])
     ),
 })
